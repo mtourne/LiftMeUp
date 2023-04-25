3587c4f06fe1930d87876e43089bfb75009b0229
Matthieu Tourne <matthieu.tourne@gmail.com>
2023-04-25 16:58:53 +0100

    adding login with google / apple button. Sign up text

--- liftme_app/lib/pages/login_page.dart
+++ liftme_app/lib/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../components/my_textfield.dart';
 import '../components/my_button.dart';
+import '../components/square_tile.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -68,58 +69,122 @@ class _LoginPageState extends State<LoginPage> {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-        appBar: AppBar(
-            title: Text('Login'),
-        ),
+        // appBar: AppBar(
+        //     title: Text('Login'),
+        // ),
 
         // safe area makes sure to avoid the screen notch
         body: SafeArea(
             child: Center(
-                child: Column(
-                    children: [
-                        SizedBox(height: 50),
-                        // logo
-                        // TODO (mtourne): couldn't make the svg format work. used PNG
-                        // SvgPicture.asset('assets/icons/logo_large-optimized.svg'), 
-                        Image.asset('assets/icons/logo_large.png', scale: 1.0),
-                        const SizedBox(height: 50),
-
-                        // welcome back
-                        const Text(
-                            "Welcome Back. We missed you!", 
-                            style: TextStyle(
-                                fontSize: 16,
-                            ),
-                        ),
-                        const SizedBox(height: 25),
-
-                        // username / password  fields
-                        MyTextField(
-                            controller: usernameController,
-                            hintText: 'Username',
-                        ),
-                        const SizedBox(height: 10),
-
-                        MyTextField(
-                            controller: passwordController,
-                            hintText: 'Password',
-                            obscureText: true,
-                        ),
-                        // TODO (mtourne): add forgot password
-
-                        const SizedBox(height: 25),
-
-                        // log in button
-                        MyButton(
-                            text: "Log in",
-                            onTap: signUserIn,
-                        ),
-
-                        // or continue with google / apple sign in.
-
-                        // not a member? sign up
+                child: SingleChildScrollView(
+                  child: Column(
+                      children: [
+                          // logo
+                          // TODO (mtourne): couldn't make the svg format work. used PNG
+                          // SvgPicture.asset('assets/icons/logo_large-optimized.svg'), 
+                          Image.asset(
+                              'assets/icons/logo_large.png', 
+                              height: 150,
+                          ),
+                          const SizedBox(height: 40),
+                
+                          // welcome back
+                          const Text(
+                              "Welcome Back. We missed you!", 
+                              style: TextStyle(
+                                  fontSize: 16,
+                              ),
+                          ),
+                          const SizedBox(height: 25),
+                
+                          // username / password  fields
+                          MyTextField(
+                              controller: usernameController,
+                              hintText: 'Username',
+                          ),
+                          const SizedBox(height: 10),
+                
+                          MyTextField(
+                              controller: passwordController,
+                              hintText: 'Password',
+                              obscureText: true,
+                          ),
+                          const SizedBox(height: 10),
+                
+                
+                          // forgot password
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                    Text("Forgot Password?",
+                                    style: TextStyle(color: Colors.grey[600]),
+                                    ),
+                            ],),
+                          ),
                     
-                    ],
+                          const SizedBox(height: 25),
+                
+                          // log in button
+                          MyButton(
+                              text: "Log in",
+                              onTap: signUserIn,
+                          ),
+                          const SizedBox(height: 40),
+                
+                          // or continue with google / apple sign in.
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  thickness: 0.5,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text('Or continue with', 
+                                  style: TextStyle(color: Colors.grey[600]),
+                                ),
+                              ),
+
+                              Expanded(
+                                child: Divider(
+                                  thickness: 0.5,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+
+                            ],
+                          ),
+                          const SizedBox(height: 40),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                SquareTile(imagePath: 'assets/icons/google.png'),
+                                SizedBox(width: 10),
+                                SquareTile(imagePath: 'assets/icons/apple.png'),
+                          ],),
+
+                          const SizedBox(height: 40),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center ,
+                            children: [
+                            Text('Not a member?'),
+                            SizedBox(width: 10),
+                            Text('Register now', style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                          ],)
+                
+                          // not a member? sign up
+                      
+                      ],
+                  ),
                 ), 
             ),
         ),
